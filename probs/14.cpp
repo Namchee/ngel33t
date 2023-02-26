bd27ee9d3120bc845fbf9f0d8f615b0bae3a194b
Cristopher Namchee <cristopher@tokopedia.com>
2023-02-26 17:37:42 +0700

    feat: add solution for problem 14

---
+++ probs/14.cpp
@@ -0,0 +1,34 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs)
+{
+    string curr = "";
+    int len = 1;
+
+    while (true)
+    {
+        if (len > strs[0].length())
+        {
+            return curr;
+        }
+
+        string ref = strs[0].substr(0, len);
+
+        for (int i = 1; i < strs.size(); i++)
+        {
+            string com = strs[i].substr(0, len);
+
+            if (com != ref)
+            {
+                return curr;
+            }
+        }
+
+        curr = ref;
+        len++;
+    }
+
+    return curr;
+}
