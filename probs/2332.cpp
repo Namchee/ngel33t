0f83ac4ad8060551eac56a920454362cd6bfe755
Cristopher Namchee <cristophernamchee12@gmail.com>
2024-03-04 13:42:24 +0700

    feat: complete 2322

--- probs/2332.cpp
+++ probs/2332.cpp
@@ -7,11 +7,13 @@ public:
         sort(buses.begin(), buses.end());
         sort(passengers.begin(), passengers.end());
 
+        int begin = 0;
+
         for (int i = 0; i < buses.size() - 1; i++) {
             int curr = capacity;
 
-            while (curr > 0 && passengers[0] <= buses[i]) {
-                passengers.pop_front();
+            while (curr > 0 && begin < passengers.size() && passengers[begin] <= buses[i]) {
+                begin++;
                 curr--;
             }
         }
@@ -19,15 +21,21 @@ public:
         vector<int> pass;
         int curr = capacity;
 
-        while (curr > 0 && passengers[0] <= buses[i]) {
-            pass.push_back(passengers[0]);
-            passengers.pop_front();
+        while (curr > 0 && begin < passengers.size() && passengers[begin] <= buses[buses.size() - 1]) {
+            pass.push_back(passengers[begin]);
+            begin++;
             curr--;
         }
 
-        pass.push_back(buses[buses.size() - 1]);
-        if (pass.size() > capacity) {
-            
+        int limit = buses[buses.size() - 1];
+        if (pass.size() == capacity) {
+            limit = pass[pass.size() - 1];
         }
+
+        while (binary_search(passengers.begin(), passengers.end(), limit)) {
+            limit--;
+        }
+
+        return limit;
     }
 };
