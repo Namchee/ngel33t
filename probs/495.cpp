10ac38bff6b50a5b5562ae0c58298d2c7605929e
Cristopher <cristophernamchee12@gmail.com>
2024-03-17 19:33:48 +0700

    feat: solve 495

---
+++ probs/495.cpp
@@ -0,0 +1,23 @@
+#include <vector>;
+#include <cmath>;
+using namespace std;
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& time, int duration) {
+        int previous = -1;
+        int answer = 0;
+
+        for (int i: time) {
+            answer += duration;
+
+            if (previous >= i) {
+                answer = answer - abs(previous - i) - 1; 
+            }
+
+            previous = i + duration - 1;
+        }
+
+        return answer;
+    }
+};
