1191f8138e5206572174cb5538fee930ec9a3299
Cristopher <cristophernamchee12@gmail.com>
2024-04-22 21:17:50 +0700

    feat: add disjoint set to library

---
+++ lib/graph/disjoint-set.cpp
@@ -0,0 +1,33 @@
+int parent[10000], rank[10000] = 0;
+
+// Disjoint Set - can be used t o check if x is connected to y
+// or counting number of connected components
+// O(log n) on average
+
+void make_set(int v) {
+    parent[v] = v;
+    rank[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v]) {
+        return v;
+    }
+
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank[b] < rank[a]) {
+            swap(a, b);
+        }
+
+        parent[b] = a;
+        if (rank[a] == rank[b]) {
+            rank[a]++;
+        }
+    }
+}
