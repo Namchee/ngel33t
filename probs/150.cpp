d92fd4618392266efdb35185fc35f14b0f7602e9
Cristopher <cristophernamchee12@gmail.com>
2024-06-04 21:10:17 +0700

    feat: solve 150

---
+++ probs/150.cpp
@@ -0,0 +1,33 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+
+        for (string str: tokens) {
+            if (str != "+" && str != "-" && str != "*" && str != "/") {
+                stk.push(stoi(str));
+            } else {
+                int b = stk.top();
+                stk.pop();
+                int a = stk.top();
+                stk.pop();
+
+                if (str == "+") {
+                    stk.push(a + b);
+                } else if (str == "-") {
+                    stk.push(a - b);
+                } else if (str == "*") {
+                    stk.push(a * b); 
+                } else {
+                    stk.push(a / b);
+                }
+            }
+        }
+
+        return stk.top();
+    }
+};
