063a39d8155be756848bccfcf91da8bdc839b23f
Cristopher <cristophernamchee12@gmail.com>
2024-06-12 07:30:39 +0700

    feat: do 75 in one pass

--- probs/75.cpp
+++ probs/75.cpp
@@ -1,29 +1,27 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zeroes = 0;
-        int ones = 0;
-        int twos = 0;
+        int zero = 0;
+        int two = nums.size() - 1;
 
-        for (int num: nums) {
-            zeroes += num == 0;
-            ones += num == 1;
-            twos += num == 2;   
-        }
+        int idx = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (zeroes) {
-                nums[i] = 0;
-                zeroes--;
-            } else if (ones) {
-                nums[i] = 1;
-                ones--;
+        while (idx <= two) {
+            if (nums[idx] == 0) {
+                swap(nums[idx], nums[zero]);
+                zero += 1;
+                if (idx < zero) {
+                    idx = zero;
+                }
+            } else if (nums[idx] == 2) {
+                swap(nums[idx], nums[two]);
+                two--;
             } else {
-                nums[i] = 2;
-                twos--;
+                idx++;
             }
         }
     }
