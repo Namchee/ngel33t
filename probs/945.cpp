905f936fcf54ac64a891bf31e87aab14156d98e4
Cristopher <cristophernamchee12@gmail.com>
2024-06-14 08:17:04 +0700

    refactor: optimize 945

--- probs/945.cpp
+++ probs/945.cpp
@@ -2,24 +2,21 @@
 #include <algorithm>
 using namespace std;
 
+
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        unordered_map<int, int> mp;
-
+        
         int n = nums.size();
+
         int sum = 0;
+        int next = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (mp.find(nums[i]) == mp.end()) {
-                mp[nums[i]] = i;
-            } else {
-                int inc = i - mp[nums[i]];
-                sum += inc;
-
-                mp[nums[i] + inc] = i;
-            }
+        for (int num: nums) {
+            next = max(num, next);
+            sum += next - num;
+            next++;
         }
 
         return sum;
