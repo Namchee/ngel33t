32a5444a068bea2354583ef43edc0146d0ea121a
Cristopher <cristophernamchee12@gmail.com>
2024-06-16 13:00:54 +0700

    refactor: simplify 235

--- probs/235.cpp
+++ probs/235.cpp
@@ -12,21 +12,20 @@ class Solution
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-        if (root == nullptr)
+        while (true)
         {
-            return nullptr;
+            if (root->val < p->val && root->val < q->val)
+            {
+                root = root->right;
+            }
+            else if (root->val > p->val && root->val > q->val)
+            {
+                root = root->left;
+            }
+            else
+            {
+                return root;
+            }
         }
-
-        if ((root->val >= p->val && root->val <= q->val) || (root->val <= p->val && root->val >= q->val))
-        {
-            return root;
-        }
-
-        if (root->val > p->val && root->val > q->val)
-        {
-            return lowestCommonAncestor(root->left, p, q);
-        }
-
-        return lowestCommonAncestor(root->right, p, q);
     }
 };
