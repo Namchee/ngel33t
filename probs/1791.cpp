c9a1e1730db29b2c62ae902143158fc44a060855
Cristopher <cristophernamchee12@gmail.com>
2024-06-27 20:02:44 +0700

    refactor: optimize 1791 to be O(1)

--- probs/1791.cpp
+++ probs/1791.cpp
@@ -1,23 +1,14 @@
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int, int> mp;
-        for (vector<int> pair: edges) {
-            mp[pair[0]]++;
-            mp[pair[1]]++;
-        }
+        vector<int> a = edges[0];
+        vector<int> b = edges[1];
 
-        int sz = mp.size();
-        for (auto it: mp) {
-            if (it.second == sz - 1) {
-                return it.first;
-            }
-        }
-
-        return -1;
+        return (a[0] == b[0]) || (a[0] == b[1])
+            ? a[0]
+            : a[1];
     }
 };
