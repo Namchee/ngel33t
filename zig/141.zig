23b549af3e6bf3d499c1b9ae9330787ad2e50589
Cristopher <cristophernamchee12@gmail.com>
2025-02-18 22:19:39 +0700

    feat: solve 141 with zig

--- zig/141.zig
+++ zig/141.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 const Node = struct {
     val: i32,
-    next: *Node,
+    next: ?*Node,
 
     pub fn init(val: i32) Node {
         return Node{
@@ -12,8 +12,56 @@ const Node = struct {
     }
 };
 
-fn solution(head: *Node) bool {}
+fn solution(head: ?*Node) bool {
+    var tortoise = head;
+    var hare = head;
+
+    while (tortoise != null and hare != null and hare.?.next != null) {
+        tortoise = tortoise.?.next;
+        hare = hare.?.next.?.next;
+
+        if (tortoise == hare) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 const expect = std.testing.expect;
 
-test "test case #1" {}
+test "test case #1" {
+    var a = Node.init(3);
+    var b = Node.init(2);
+    var c = Node.init(0);
+    var d = Node.init(-4);
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &b;
+
+    const sol = solution(&a);
+
+    try expect(sol);
+}
+
+test "test case #2" {
+    var a = Node.init(1);
+    var b = Node.init(2);
+
+    a.next = &b;
+    b.next = &a;
+
+    const sol = solution(&a);
+
+    try expect(sol);
+}
+
+test "test case #3" {
+    var a = Node.init(1);
+
+    const sol = solution(&a);
+
+    try expect(!sol);
+}
