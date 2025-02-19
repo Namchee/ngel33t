6a222b0d284d22c2041d09c9467386b5af964d76
Cristopher <cristophernamchee12@gmail.com>
2025-02-19 22:09:37 +0700

    chore: try 92

---
+++ zig/92.zig
@@ -0,0 +1,77 @@
+const std = @import("std");
+
+const Node = struct {
+    val: i32,
+    next: ?*Node,
+
+    pub fn init(alloc: std.mem.Allocator, val: i32) !*Node {
+        const node = try alloc.create(Node);
+        node.val = val;
+        node.next = null;
+
+        return node;
+    }
+};
+
+fn solution(alloc: std.mem.Allocator, head: ?*Node, left: usize, right: usize) !?*Node {
+    if (head == null or left == right) {
+        return head;
+    }
+
+    var dummy = try Node.init(alloc, -1);
+    dummy.next = head;
+    defer alloc.destroy(dummy);
+
+    var prev: ?*Node = dummy;
+
+    for (0..left - 1) |_| {
+        prev = prev.?.next;
+    }
+
+    var curr: ?*Node = prev.?.next;
+
+    for (0..right - left) |_| {
+        var temp: ?*Node = curr;
+        curr.?.next = temp.?.next;
+        temp.?.next = prev.?.next;
+        prev.?.next = temp;
+    }
+
+    return dummy.next;
+}
+
+const expect = std.testing.expect;
+
+test "test case #1" {
+    const alloc = std.testing.allocator;
+
+    const content = [_]i32{ 1, 2, 3, 4, 5 };
+    var sentinel: ?*Node = try Node.init(alloc, -1);
+    defer {
+        while (sentinel) |node| {
+            const next = node.next;
+            alloc.destroy(node);
+            sentinel = next;
+        }
+    }
+
+    var runner: ?*Node = sentinel;
+    for (content) |num| {
+        runner.?.next = try Node.init(alloc, num);
+        runner = runner.?.next;
+    }
+
+    const sol = try solution(alloc, sentinel.?.next, 2, 4);
+
+    const expected = [_]i32{ 1, 4, 3, 2, 5 };
+    runner = sol;
+
+    for (expected) |num| {
+        try expect(num == runner.?.val);
+        runner = runner.?.next;
+    }
+}
+
+test "test case #2" {}
+
+test "test case #3" {}
