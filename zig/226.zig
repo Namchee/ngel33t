28de09919e9b26e21e34bc9388e28d049bf96cfc
Cristopher <cristophernamchee12@gmail.com>
2025-02-21 14:10:39 +0700

    feat: solve 226 with zig

---
+++ zig/226.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+const TreeNode = struct {
+    val: i32,
+    left: ?*TreeNode = null,
+    right: ?*TreeNode = null,
+
+    pub fn init(alloc: std.mem.Allocator, val: i32) !*TreeNode {
+        const node = try alloc.create(TreeNode);
+        node.val = val;
+
+        return node;
+    }
+};
+
+fn solution(node: ?*TreeNode) void {
+    if (node) |n| {
+        const temp = n.left;
+        n.left = n.right;
+        n.right = temp;
+
+        solution(n.left);
+        solution(n.right);
+    }
+}
+
+const expect = std.testing.expect;
+
+test "test case #1" {
+    const alloc = std.testing.allocator;
+
+    var a = try TreeNode.init(alloc, 2);
+    const b = try TreeNode.init(alloc, 1);
+    const c = try TreeNode.init(alloc, 3);
+
+    defer alloc.destroy(a);
+    defer alloc.destroy(b);
+    defer alloc.destroy(c);
+
+    a.left = b;
+    a.right = c;
+
+    solution(a);
+
+    try expect(a.left.?.val == 3);
+    try expect(a.right.?.val == 1);
+}
+
+test "test case #2" {
+    // const alloc = std.testing.allocator;
+}
