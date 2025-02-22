18bdddac0411170a407dc1bb7c4d821cea41e2d2
Cristopher <cristophernamchee12@gmail.com>
2025-02-22 12:39:34 +0700

    feat: solve 101 with zig

--- zig/101.zig
+++ zig/101.zig
@@ -2,30 +2,42 @@ const std = @import("std");
 
 const TreeNode = struct {
     val: i32,
-    left: ?*TreeNode = null,
-    right: ?*TreeNode = null,
+    left: ?*TreeNode,
+    right: ?*TreeNode,
 
     pub fn init(alloc: std.mem.Allocator, val: i32) !*TreeNode {
         const node = try alloc.create(TreeNode);
         node.val = val;
+        node.left = null;
+        node.right = null;
 
         return node;
     }
 };
 
-fn solution(a: ?*TreeNode, b: ?*TreeNode) bool {
-    if (a == null and b == null) {
-        return true;
+fn traverse(node: ?*TreeNode, list: *std.ArrayList(i32)) !void {
+    if (node) |n| {
+        try traverse(n.left, list);
+        try list.append(n.val);
+        try traverse(n.right, list);
+    }
+}
+
+fn solution(alloc: std.mem.Allocator, root: ?*TreeNode) !bool {
+    var list = std.ArrayList(i32).init(alloc);
+    defer list.deinit();
+
+    try traverse(root, &list);
+
+    for (0..list.items.len / 2) |idx| {
+        const opp = list.items.len - 1 - idx;
+
+        if (list.items[idx] != list.items[opp]) {
+            return false;
+        }
     }
 
-    if (a == null or b == null) {
-        return false;
-    }
-
-    const nodeA = a.?;
-    const nodeB = b.?;
-
-    return nodeA.val == nodeB.val and solution(nodeA.left, nodeB.left) and solution(nodeA.right, nodeB.right);
+    return true;
 }
 
 const except = std.testing.expect;
@@ -34,48 +46,57 @@ test "test case #1" {
     const alloc = std.testing.allocator;
 
     var a = try TreeNode.init(alloc, 1);
-    const b = try TreeNode.init(alloc, 2);
-    var c = try TreeNode.init(alloc, 1);
-    const d = try TreeNode.init(alloc, 2);
+    var b = try TreeNode.init(alloc, 2);
+    const c = try TreeNode.init(alloc, 3);
+    const d = try TreeNode.init(alloc, 4);
+
+    var e = try TreeNode.init(alloc, 2);
+    const f = try TreeNode.init(alloc, 4);
+    const g = try TreeNode.init(alloc, 3);
 
     a.left = b;
-    c.right = d;
+    b.left = c;
+    b.right = d;
+
+    a.right = e;
+    e.left = f;
+    e.right = g;
 
     defer alloc.destroy(a);
     defer alloc.destroy(b);
     defer alloc.destroy(c);
     defer alloc.destroy(d);
+    defer alloc.destroy(e);
+    defer alloc.destroy(f);
+    defer alloc.destroy(g);
 
-    const sol = solution(a, b);
+    const sol = try solution(alloc, a);
 
-    try except(!sol);
+    try except(sol);
 }
 
 test "test case #2" {
     const alloc = std.testing.allocator;
 
     var a = try TreeNode.init(alloc, 1);
-    const b = try TreeNode.init(alloc, 2);
+    var b = try TreeNode.init(alloc, 2);
     const c = try TreeNode.init(alloc, 3);
 
-    var d = try TreeNode.init(alloc, 1);
-    const e = try TreeNode.init(alloc, 2);
-    const f = try TreeNode.init(alloc, 3);
+    var d = try TreeNode.init(alloc, 2);
+    const e = try TreeNode.init(alloc, 3);
 
     a.left = b;
-    a.right = c;
+    b.right = c;
+    a.right = d;
+    d.right = e;
 
-    d.left = e;
-    d.right = f;
-
-    defer alloc.destroy(e);
-    defer alloc.destroy(f);
+    defer alloc.destroy(a);
     defer alloc.destroy(b);
     defer alloc.destroy(c);
-    defer alloc.destroy(a);
     defer alloc.destroy(d);
+    defer alloc.destroy(e);
 
-    const sol = solution(a, d);
+    const sol = try solution(alloc, a);
 
-    try except(sol);
+    try except(!sol);
 }
