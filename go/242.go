508b47d813d8f57941cf04aee225ce259dc0bae0
Cristopher <cristophernamchee12@gmail.com>
2026-02-17 13:37:45 +0700

    feat: add solution for 242.zig

---
+++ go/242.go
@@ -0,0 +1,32 @@
+package main
+
+func isAnagram(s string, t string) bool {
+	// rune is used to handle unicode
+	mp := map[rune]int{}
+
+	for _, val := range s {
+		_, ok := mp[val]
+		if ok {
+			mp[val] += 1
+		} else {
+			mp[val] = 1
+		}
+	}
+
+	for _, chr := range t {
+		val, ok := mp[chr]
+		if ok {
+			if val == 1 {
+				delete(mp, chr)
+			} else {
+				mp[chr] -= 1
+			}
+
+			continue
+		}
+
+		return false
+	}
+
+	return len(mp) == 0
+}
