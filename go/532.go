6c7c594bce5906bac9744401fcd3914a4c797172
Cristopher <cristophernamchee12@gmail.com>
2026-02-28 11:25:44 +0700

    refactor: simplify 532.go

--- go/532.go
+++ go/532.go
@@ -1,54 +1,28 @@
 package main
 
-import (
-	"slices"
-)
-
 func findPairs(nums []int, k int) int {
-	result := 0
-	mp := map[int]int{}
-	used := map[int]bool{}
+	if k < 0 {
+		return 0
+	}
 
-	slices.Sort(nums)
+	count := 0
+	mp := make(map[int]int)
 
 	for _, num := range nums {
 		mp[num]++
 	}
 
-	if k == 0 {
-		keys := make([]int, 0, len(mp))
-		for k2 := range mp {
-			keys = append(keys, k2)
-		}
-
-		for _, key := range keys {
-			freq := mp[key]
-
-			result += freq / 2
-		}
-	} else {
-		for _, num := range nums {
-			_, use := used[num]
-			if use {
-				continue
+	for num, freq := range mp {
+		if k == 0 {
+			if freq >= 2 {
+				count++
 			}
-
-			lt := num - k
-			_, ok := mp[lt]
-			_, u := used[lt]
-			if ok && !u {
-				result++
-				used[lt] = true
-			}
-
-			gt := num + k
-			_, ok = mp[gt]
-			if ok {
-				result++
-				used[num] = true
+		} else {
+			if _, ok := mp[num+k]; ok {
+				count++
 			}
 		}
 	}
 
-	return result
+	return count
 }
