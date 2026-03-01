48730ecd4d2888ac5c7242453378b6728f4f80a3
Cristopher <cristophernamchee12@gmail.com>
2026-03-01 15:23:11 +0700

    feat: write solution for 784.go

---
+++ go/784.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func generateLetterCase(idx int, tokens []string, result *[]string) {
+	if idx >= len(tokens) {
+		*result = append(*result, strings.Join(tokens, ""))
+		return
+	}
+
+	if _, err := strconv.Atoi(tokens[idx]); err == nil {
+		generateLetterCase(idx+1, tokens, result)
+	} else {
+		tokens[idx] = strings.ToUpper(tokens[idx])
+		generateLetterCase(idx+1, tokens, result)
+		tokens[idx] = strings.ToLower(tokens[idx])
+
+		generateLetterCase(idx+1, tokens, result)
+
+	}
+}
+
+func letterCasePermutation(s string) []string {
+	result := []string{}
+	tokens := strings.Split(s, "")
+
+	generateLetterCase(0, tokens, &result)
+
+	return result
+}
