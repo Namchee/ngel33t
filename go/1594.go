3c645429b29fc7e568c9f96edae95ce46db45227
Cristopher <cristophernamchee12@gmail.com>
2026-03-23 09:12:55 +0700

    refactor: write 1594 with my own way

--- go/1594.go
+++ go/1594.go
@@ -1,51 +1,45 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
-const ProductMod = 1_000_000_007
+const ProductMod = 1000000007
 
 func maxProductPath(grid [][]int) int {
 	m := len(grid)
 	n := len(grid[0])
 
-	matrix := make([][][]int64, m)
+	result := -1
+	matrix := make([][][]int, m)
 	for i := 0; i < m; i++ {
-		matrix[i] = make([][]int64, n)
+		matrix[i] = make([][]int, n)
 	}
+
+	matrix[0][0] = []int{grid[0][0], grid[0][0]}
 
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
-			if i == 0 && j == 0 {
-				matrix[i][j] = []int64{int64(grid[i][j]), int64(grid[i][j])}
-				continue
-			}
+			a := math.MinInt32
+			b := math.MaxInt32
 
-			var curMax int64 = math.MinInt64
-			var curMin int64 = math.MaxInt64
-
+			// from the top lane
 			if i > 0 {
-				v1 := int64(grid[i][j]) * matrix[i-1][j][0]
-				v2 := int64(grid[i][j]) * matrix[i-1][j][1]
-				curMax = max(curMax, v1, v2)
-				curMin = min(curMin, v1, v2)
+				a = max(a, (grid[i][j] * matrix[i-1][j][0]), (grid[i][j] * matrix[i-1][j][1]))
+				b = min(b, (grid[i][j] * matrix[i-1][j][0]), (grid[i][j] * matrix[i-1][j][1]))
 			}
 
+			// from the left lane
 			if j > 0 {
-				v1 := int64(grid[i][j]) * matrix[i][j-1][0]
-				v2 := int64(grid[i][j]) * matrix[i][j-1][1]
-				curMax = max(curMax, v1, v2)
-				curMin = min(curMin, v1, v2)
+				a = max(a, (grid[i][j] * matrix[i][j-1][0]), (grid[i][j] * matrix[i][j-1][1]))
+				b = min(b, (grid[i][j] * matrix[i][j-1][0]), (grid[i][j] * matrix[i][j-1][1]))
 			}
 
-			matrix[i][j] = []int64{curMax, curMin}
+			if len(matrix[i][j]) == 0 {
+				matrix[i][j] = []int{a, b}
+			}
 		}
 	}
 
-	result := matrix[m-1][n-1][0]
-
-	if result < 0 {
-		return -1
-	}
-
-	return int(result % ProductMod)
+	return max(result, matrix[m-1][n-1][0]%ProductMod, matrix[m-1][n-1][1]%ProductMod)
 }
