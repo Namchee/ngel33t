d606069a84add68f70e07b07d2c45101677fdcba
Cristopher <cristophernamchee12@gmail.com>
2026-03-27 22:32:13 +0700

    fix: optimize 2946.zig without allocator

--- zig/2946.zig
+++ zig/2946.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
 fn solution(comptime M: usize, comptime N: usize, mat: [M][N]i32, k: usize) bool {
-    var grid = [M][N]i32{};
-    for (0..M) |i| {
-        grid[i] = [_]i32{0} ** N;
-    }
+    var grid: [M][N]i32 = [_][N]i32{[_]i32{0} ** N} ** M;
+
+    const nI = @as(i32, @intCast(N));
+    const kI = @as(i32, @intCast(k));
 
     for (0..M) |i| {
         if (@mod(i, 2) == 0) {
@@ -14,7 +14,7 @@ fn solution(comptime M: usize, comptime N: usize, mat: [M][N]i32, k: usize) bool
             }
         } else {
             for (0..N) |j| {
-                const pos = @mod(@mod(@as(i32, @intCast(j)) - @as(i32, @intCast(k)), N) + N, N);
+                const pos = @mod(@mod(@as(i32, @intCast(j)) - kI, nI) + nI, nI);
 
                 grid[i][@as(usize, @intCast(pos))] = mat[i][j];
             }
@@ -44,6 +44,23 @@ test "test case #1" {
         .{ 7, 8, 9 },
     };
     const k = 4;
+
+    const expected = false;
+    const actual = solution(M, N, mat, k);
+
+    try expect(expected, actual);
+}
+
+test "test case #2" {
+    const M = 3;
+    const N = 4;
+
+    const mat = [M][N]i32{
+        .{ 1, 2, 1, 2 },
+        .{ 5, 5, 5, 5 },
+        .{ 3, 6, 3, 6 },
+    };
+    const k = 3;
 
     const expected = false;
     const actual = solution(M, N, mat, k);
