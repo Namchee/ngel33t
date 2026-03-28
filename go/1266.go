dc9a6dc38148d6bfca8ea92880117143b0d68a55
Cristopher <cristophernamchee12@gmail.com>
2026-03-28 11:03:42 +0700

    refactor: optimize 1266.go using Chebyshev distance

--- go/1266.go
+++ go/1266.go
@@ -1,5 +1,23 @@
 package main
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func chebyshevDistance(a []int, b []int) int {
+	return max(abs(a[0]-b[0]), abs(a[1]-b[1]))
+}
+
 func minTimeToVisitAllPoints(points [][]int) int {
 	result := 0
 
