0d2743e7d6b0c8a795333685a6a02ed30fdb626c
Cristopher <cristophernamchee12@gmail.com>
2026-03-29 14:12:10 +0700

    feat: write solution for 316.go

--- go/316.go
+++ go/316.go
@@ -1,49 +1,26 @@
 package main
 
-func generateUSLS(pos map[rune][]int, last int, members map[rune]bool, curr []rune) string {
-	if len(members) == 0 {
-		return string(curr)
-	}
-
-	for i := 0; i < 26; i++ {
-		ch := rune(i + 'a')
-		_, ok := members[ch]
-
-		// not used yet
-		if ok {
-			for _, idx := range pos[ch] {
-				if idx > last {
-					delete(members, ch)
-					newCurr := append(curr, ch)
-					attempt := generateUSLS(pos, idx, members, newCurr)
-
-					if len(attempt) > 0 {
-						return attempt
-					}
-
-					members[ch] = true
-				}
-			}
-		}
-
-	}
-
-	// shouldn't be possible
-	return ""
-}
-
 func removeDuplicateLetters(s string) string {
-	mp := map[rune][]int{}
-	members := map[rune]bool{}
-
-	for i := 0; i < 26; i++ {
-		mp[rune(i+'a')] = []int{}
+	occ := map[rune]int{}
+	for idx, ch := range s {
+		occ[ch] = idx
 	}
+
+	stack := []rune{}
+	seen := map[rune]bool{}
 
 	for idx, ch := range s {
-		mp[ch] = append(mp[ch], idx)
-		members[ch] = true
+		if !seen[ch] {
+			for len(stack) > 0 && ch < stack[len(stack)-1] && occ[stack[len(stack)-1]] > idx {
+				top := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				seen[top] = false
+			}
+
+			stack = append(stack, ch)
+			seen[ch] = true
+		}
 	}
 
-	return generateUSLS(mp, -1, members, []rune{})
+	return string(stack)
 }
