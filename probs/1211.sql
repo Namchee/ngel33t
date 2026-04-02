dde2ce70b0f7fd3874b9bb973b6b142cb4322506
Cristopher <cristophernamchee12@gmail.com>
2026-04-02 20:52:24 +0700

    feat: write solution for 1211.sql

---
+++ probs/1211.sql
@@ -0,0 +1,11 @@
+SELECT
+    q.query_name,
+    ROUND(AVG(q.rating / q.position), 2) AS quality,
+    ROUND(
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS poor_query_percentage
+FROM
+    Queries q
+GROUP BY
+    q.query_name;
