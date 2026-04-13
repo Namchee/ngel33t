7c317422933a1d243449bc4ce38bdacc1f1b815b
Cristopher <cristophernamchee12@gmail.com>
2026-04-13 19:47:11 +0700

    feat: write solution for 1848.zig

--- zig/1848.zig
+++ zig/1848.zig
@@ -13,3 +13,36 @@ fn solution(nums: []const i32, target: i32, start: usize) usize {
 }
 
 const expect = std.testing.expectEqual;
+
+test "test case #1" {
+    const nums = [_]i32{ 1, 2, 3, 4, 5 };
+    const target = 5;
+    const start = 3;
+
+    const expected = 1;
+    const actual = solution(&nums, target, start);
+
+    try expect(expected, actual);
+}
+
+test "test case #2" {
+    const nums = [_]i32{1};
+    const target = 1;
+    const start = 0;
+
+    const expected = 0;
+    const actual = solution(&nums, target, start);
+
+    try expect(expected, actual);
+}
+
+test "test case #3" {
+    const nums = [_]i32{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    const target = 1;
+    const start = 0;
+
+    const expected = 0;
+    const actual = solution(&nums, target, start);
+
+    try expect(expected, actual);
+}
