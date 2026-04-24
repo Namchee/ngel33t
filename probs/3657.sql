93f9d2e361f5eb7904cdf2b5e15d67cf4b198ad3
Cristopher <cristophernamchee12@gmail.com>
2026-04-24 09:37:59 +0700

    feat: write solution for 3657.sql

---
+++ probs/3657.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    customer_id
+FROM
+    customer_transactions
+GROUP BY
+    customer_id
+HAVING
+    COUNT(CASE WHEN transaction_type = 'purchase' THEN 1 END) >= 3 AND
+    DATEDIFF(MAX(transaction_date), MIN(transaction_date)) > 29 AND
+    COUNT(CASE WHEN transaction_type = 'refund' THEN 1 END) / COUNT(transaction_id) < 0.2
+ORDER BY
+    customer_id
