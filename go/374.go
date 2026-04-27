81a9e47a81b5da4fc7f5909296e362ea14f71a06
Cristopher <cristophernamchee12@gmail.com>
2026-04-27 21:16:08 +0700

    feat: write solution for 374.go

---
+++ go/374.go
@@ -0,0 +1,32 @@
+package main
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	left := 0
+	right := n
+
+	for left <= right {
+		mid := left + (right-left)/2
+		res := guess(mid)
+
+		if res == 0 {
+			return mid
+		}
+
+		if res == -1 {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left + (right-left)/2
+}
